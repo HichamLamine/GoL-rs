efbc1f8953ae766def4ab6bafda4759c047ded79
HichamLamine <hichamlamine29@gmail.com>
2024-03-17 21:07:10 +0000

    implemented the logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cell;
+
 use ggez::{
     conf::{WindowMode, WindowSetup},
     event::{self, EventHandler},
@@ -5,10 +7,11 @@ use ggez::{
     winit::dpi::Pixel,
     Context, ContextBuilder, GameResult,
 };
+use rand::{thread_rng, Rng};
 
-const CELL_SIZE: f32 = 20.;
-const GRID_WIDTH: f32 = 50.;
-const GRID_HEIGHT: f32 = 50.;
+const CELL_SIZE: f32 = 10.;
+const GRID_WIDTH: f32 = 100.;
+const GRID_HEIGHT: f32 = 100.;
 const WINDOW_WIDTH: f32 = CELL_SIZE * GRID_WIDTH;
 const WINDOW_HEIGHT: f32 = CELL_SIZE * GRID_HEIGHT;
 
@@ -24,7 +27,42 @@ impl GameState {
         Self {
             grid: vec![vec![false; GRID_HEIGHT as usize]; GRID_WIDTH as usize],
             generation: 0,
-            target_fps: 1,
+            target_fps: 20,
+        }
+    }
+
+    fn update_grid_state(&mut self) {
+        let mut coords: Vec<(i32, i32)> = vec![];
+        for i in 0..GRID_WIDTH as i32 {
+            for j in 0..GRID_HEIGHT as i32 {
+                let cell_state = self.grid[i as usize][j as usize] as i8;
+                let mut live_neighbours = 0;
+                for x in -1..=1 {
+                    for y in -1..=1 {
+                        let new_x = i + x;
+                        let new_y = j + y;
+
+                        if new_x > 0
+                            && new_y > 0
+                            && new_x < GRID_WIDTH as i32
+                            && new_y < GRID_HEIGHT as i32
+                        {
+                            live_neighbours += self.grid[new_x as usize][new_y as usize] as i8;
+                        }
+                    }
+                }
+
+                live_neighbours -= cell_state;
+
+                if cell_state == 1 && (live_neighbours < 2 || live_neighbours > 3) {
+                    coords.push((i, j));
+                } else if cell_state == 0 && live_neighbours == 3 {
+                    coords.push((i, j));
+                }
+            }
+        }
+        for coord in coords.iter() {
+            self.grid[coord.0 as usize][coord.1 as usize] ^= true;
         }
     }
 }
@@ -32,7 +70,7 @@ impl GameState {
 impl EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         if ctx.time.check_update_time(self.target_fps) {
-            println!("Hi");
+            self.update_grid_state();
         }
         Ok(())
     }
@@ -58,6 +96,22 @@ impl EventHandler for GameState {
                         DrawParam::default(),
                     );
                 }
+                // else {
+                //     canvas.draw(
+                //         &Mesh::new_rectangle(
+                //             ctx,
+                //             DrawMode::stroke(1.),
+                //             Rect::new(
+                //                 (i * CELL_SIZE as i32) as f32,
+                //                 (j * CELL_SIZE as i32) as f32,
+                //                 CELL_SIZE,
+                //                 CELL_SIZE,
+                //             ),
+                //             Color::WHITE,
+                //         )?,
+                //         DrawParam::default(),
+                //     );
+                // }
             }
         }
 
@@ -74,7 +128,16 @@ fn main() -> GameResult {
         .build()?;
 
     let mut game = GameState::new(&mut ctx);
-    game.grid[0][3] = true;
+
+    let generated_cells: u32 = 2000;
+    let mut rng = thread_rng();
+
+    for i in 0..generated_cells {
+        let random_x: usize = rng.gen_range(1..GRID_WIDTH as usize - 1);
+        let random_y: usize = rng.gen_range(1..GRID_HEIGHT as usize - 1);
+        game.grid[random_x][random_y] = true;
+    }
+
     event::run(ctx, event_loop, game)
 }
 
